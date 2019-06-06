d075adc42bf0d85504796d32c055a8f0233fdc8c
VukasinVujic <vukasinvujic021@gmail.com>
2019-06-06 12:40:01 +0200

    ubacivanje sa linuxa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel kdsfjsdkfjs
                 </div>
 
                 <div class="links">
